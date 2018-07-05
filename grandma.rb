a1b272f2bc57f30e90837e6b99b00b6dab93c2f0
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-05 21:36:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string == string.upcase
-    if string == "I LOVE YOU GRANDMA!"
+  if string === string.upcase
+    if string === "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
     else
       puts "NO, NOT SINCE 1938!"
